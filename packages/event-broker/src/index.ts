a908f83a3e4cbd56c64cbfb7fb92f5191abeb6be
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-05-07 20:18:26 +0200

    docs: add jsdoc

--- packages/event-broker/src/index.ts
+++ packages/event-broker/src/index.ts
@@ -4,21 +4,52 @@ export type StartID = string;
 const generateStartID = (): StartID => 
     `_ID${Math.floor(Math.random()*Date.now())}@${Date.now()}_`
 
-export type BaseEvent = Record<string,any> 
+
+
+
+/**
+ * BaseEvent is a type alias for a Record with string keys and any values.
+ */
+export type BaseEvent = Record<string, any>;
     
+/**
+ * Interface for an event submitter.
+ * @template ListenEvent The type of the event to listen for.
+ * @template ReplyEvent The type of the event to reply with.
+ */
 export interface EventSubmitter<ListenEvent extends BaseEvent, ReplyEvent extends BaseEvent> {
+    /**
+     * Sends an event and returns an iterator result.
+     * @param event The event to send.
+     * @returns An iterator result containing the reply event or undefined.
+     */
     send( event: ListenEvent ):IteratorResult<ReplyEvent | undefined, any>;
     
+    /**
+     * Sends an event and waits for a reply.
+     * @param event The event to send.
+     * @returns The reply event.
+     */
     sendAndWaitForReply( event: ListenEvent ):ReplyEvent;
 
 }
 
+/**
+ * EventBroker class that implements EventSubmitter interface.
+ * @template ListenEvent The type of the event to listen for.
+ * @template ReplyEvent The type of the event to reply with.
+ */
 export class EventBroker<ListenEvent extends BaseEvent, ReplyEvent extends BaseEvent = ListenEvent> implements EventSubmitter<ListenEvent, ReplyEvent> {
 
     private _listener?: Generator<ReplyEvent|undefined, any, ListenEvent> 
 
     private _startId?:StartID
 
+    /**
+     * Starts listening for events.
+     * @param handler The function to handle the event.
+     * @returns The start ID.
+     */
     start( handler: ( event:ListenEvent ) => ReplyEvent|void ):StartID|undefined {
         if( this._listener ) return
 
@@ -49,15 +80,32 @@ export class EventBroker<ListenEvent extends BaseEvent, ReplyEvent extends BaseE
         
     }
 
+    /**
+     * Sends an event and returns an iterator result.
+     * @param event The event to send.
+     * @returns An iterator result containing the reply event or undefined.
+     */
     send( event: ListenEvent ):IteratorResult<ReplyEvent | undefined, any>  {
         if( !this._listener ) return { value: undefined }
         return (this._listener.next(event))
     }
     
+    /**
+     * Sends an event and waits for a reply.
+     * @param event The event to send.
+     * @returns The reply event.
+     */
     sendAndWaitForReply( event: ListenEvent ):any {
         return this.send( event ).value
     }
 
+    /**
+     * Stops listening for events.
+     * @param startId The start ID.
+     * @param value The value to return.
+     * @returns An iterator result containing the reply event or undefined.
+     * @throws An error if the start ID does not match.
+     */
     stop( startId:StartID, value?: unknown  ):IteratorResult<ReplyEvent | undefined, any>|undefined {
         if( !this._listener ) return
         if( startId !== this._startId ) {
@@ -69,23 +117,51 @@ export class EventBroker<ListenEvent extends BaseEvent, ReplyEvent extends BaseE
     }
 }
 
+/**
+ * Interface for an async event submitter.
+ * @template ListenEvent The type of event to listen for.
+ * @template ReplyEvent The type of event to reply with.
+ */
 export interface AsyncEventSubmitter<ListenEvent extends BaseEvent, ReplyEvent extends BaseEvent> {
+    /**
+     * Sends an event and returns an iterator result.
+     * @param event The event to send.
+     * @returns A promise containing an iterator result containing the reply event or undefined.
+     */
     send( event: ListenEvent ):Promise<IteratorResult<ReplyEvent | undefined, any>>;
     
+    /**
+     * Sends an event and waits for a reply.
+     * @param event The event to send.
+     * @returns A promise containing the reply event.
+     */
     sendAndWaitForReply( event: ListenEvent ):Promise<ReplyEvent>;
 
 }
 
+/**
+ * An asynchronous event broker.
+ * @template ListenEvent The type of event to listen for.
+ * @template ReplyEvent The type of event to reply with.
+ */
 export class AsyncEventBroker<ListenEvent extends BaseEvent, ReplyEvent extends BaseEvent = ListenEvent> implements AsyncEventSubmitter<ListenEvent,ReplyEvent>{
 
     private _listener?: AsyncGenerator<ReplyEvent|undefined, any, ListenEvent> 
 
     private _startId?:StartID
     
+    /**
+     * Starts listening for events.
+     * @param handler The event handler.
+     * @returns A promise containing the start ID or undefined.
+     */
     async start( handler: ( event:ListenEvent ) => Promise<ReplyEvent|void> ):Promise<StartID|undefined> {
 
         if( this._listener ) return 
         
+        /**
+         * An asynchronous generator function that listens for events.
+         */
         async function* listener():AsyncGenerator<ReplyEvent|undefined, any, ListenEvent> {
         
             console.debug("async start listening...!");
@@ -112,15 +188,32 @@ export class AsyncEventBroker<ListenEvent extends BaseEvent, ReplyEvent extends 
         
     }
 
+    /**
+     * Sends an event and returns an iterator result.
+     * @param event The event to send.
+     * @returns A promise containing an iterator result containing the reply event or undefined.
+     */
     async send( event: ListenEvent ):Promise<IteratorResult<ReplyEvent | undefined, any>> {
         if( !this._listener ) return { value: undefined }
         return (await this._listener.next(event))
     }
     
+    /**
+     * Sends an event and waits for a reply.
+     * @param event The event to send.
+     * @returns A promise containing the reply event.
+     */
     async sendAndWaitForReply( event: ListenEvent ):Promise<ReplyEvent> {
        return (await this.send( event)).value
     }
 
+    /**
+     * Stops listening for events.
+     * @param startId The start ID.
+     * @param value The value to return.
+     * @returns An iterator result containing the reply event or undefined.
+     * @throws An error if the start ID does not match.
+     */
     async stop( startId:StartID, value?: unknown  ):Promise<IteratorResult<ReplyEvent | undefined, any>|undefined> {
         if( !this._listener ) return
         if( startId !== this._startId ) {
@@ -131,5 +224,4 @@ export class AsyncEventBroker<ListenEvent extends BaseEvent, ReplyEvent extends 
         return await l.return( value ) 
     }
 }
-
 
