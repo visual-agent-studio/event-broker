98ae9d4cd0b2fc178036bbabe6034ac21fe27424
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-06-03 20:49:37 +0200

    fix: export type separately
    SO: https://stackoverflow.com/a/69771178

--- packages/event-broker/src/index.ts
+++ packages/event-broker/src/index.ts
@@ -6,17 +6,10 @@
  * @license MIT
  */
 
-export { 
-    ListenerID,
-    BaseEvent,
-    AsyncEventHandler,
-    AsyncEventBroker, 
-} from './broker'
-
-export {
-    EventHandler,
-    EventBroker
-} from './broker-s'
+export { AsyncEventBroker } from './broker'
+export type { ListenerID, BaseEvent, AsyncEventHandler } from './broker'
+export { EventBroker } from './broker-s'
+export type { EventHandler } from './broker-s'
 
 export * from './broker-ml'
 export * from './broker-topics-ml'
