11df0e8ec2cfaba5dcebd7e68142c16a49908941
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-06-03 20:52:05 +0200

    build: upgrade version

--- packages/event-broker/src/index.ts
+++ packages/event-broker/src/index.ts
@@ -2,7 +2,7 @@
  * @file index.ts
  * @description This file serves as the entry point for the event-broker package, re-exporting various modules and components.
  * @module EventBrokerIndex
- * @version 2.0.0-beta-20240603-2
+ * @version 2.0.0-beta-20240603-3
  * @license MIT
  */
 
